be209b854bf557ee46508e3c1bbe48d246620b28
prahladinala <prahlad2018@gmail.com>
2021-07-31 22:50:31 +0530

    Added Files

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, Response
+import cv2
+
+app = Flask(__name__)
+camera = cv2.VideoCapture(0)
+
+
+def generate_frames():
+    while True:
+        # Read The Camera Frame
+        success, frame = camera.read()
+        # IF Failed
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+        # yield => helps to return each frame intead return frame returns only one frame
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/video')
+def video():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
